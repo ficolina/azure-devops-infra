30f23e615a1293341ad3ace980db67daeb89e89b
ficolina <95408493+ficolina@users.noreply.github.com>
2026-01-18 23:06:22 +0100

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,11 @@ output "resource_group_name" {
 output "vnet_name" {
   value = azurerm_virtual_network.vnet.name
 }
+
+output "vm_public_ip" {
+  value = azurerm_public_ip.pip.ip_address
+}
+
+output "ssh_command" {
+  value = "ssh ${var.vm_admin_username}@${azurerm_public_ip.pip.ip_address}"
+}
